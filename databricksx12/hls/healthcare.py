8e7b9fef7153891224f22193723e8d078ba82f3a
Justin Thomas <justinthomas@justinthomas.org>
2026-07-25 11:47:24 -0500

    Fix duplicate entry for key '224' in healthcare.py

--- databricksx12/hls/healthcare.py
+++ databricksx12/hls/healthcare.py
@@ -10,7 +10,6 @@ class HealthcareManager(EDI):
             "221": Remittance, # Remittance "835"
             "222": Claim837p,
             "223": Claim837i,
-            "224": None #Dental,
             "224": None #Dental 
     }
 
