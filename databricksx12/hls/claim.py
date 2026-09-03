efcaa24c018478f93f221801ad9968544f5f0885
Justin Thomas <jthomas@mohospitals.org>
2026-09-03 11:39:25 -0500

    adding medical record number and social security number to subscriber and patient loops

--- databricksx12/hls/claim.py
+++ databricksx12/hls/claim.py
@@ -311,7 +311,8 @@ class MedicalClaim(EDI):
             dmg = self._first(l, "DMG"),            
             pat = self._first(l, "PAT"),
             sbr = self._first(l, "SBR"),
-            ref = self._first([x for x in l if x.element(1) == "EA"], "REF")
+            ref_ea = self._first([x for x in l if x.element(1) == "EA"], "REF"),
+            ref_sy = self._first([x for x in l if x.element(1) == "SY"], "REF")
         )
     
     def _populate_patient_loop(self) -> Dict[str, str]:
@@ -324,7 +325,9 @@ class MedicalClaim(EDI):
             dmg = self._first(self.patient_loop, "DMG"),
             pat = self._first(self.patient_loop, "PAT"),
             sbr = self._first(self.patient_loop, "SBR"),
-            ref = self._first([x for x in self.patient_loop if x.element(1) == "EA"], "REF"))
+            ref_ea = self._first([x for x in self.patient_loop if x.element(1) == "EA"], "REF"),
+            ref_sy = self._first([x for x in self.patient_loop if x.element(1) == "SY"], "REF")
+        )
     
     def _populate_claim_loop(self):
         return ClaimIdentity(clm = self._first(self.claim_loop, "CLM"),
